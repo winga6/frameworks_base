38b3103c3120bcf8b57c598b96694ae36ece51eb
westcripp <korukaltan@gmail.com>
2016-03-22 02:00:01 +0200

    fix build

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -290,7 +290,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             mDialog.getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_DISABLE_EXPAND); 
         }  
 	
-    }
 
     private int getPowermenuAnimations() {
         return Settings.System.getInt(mContext.getContentResolver(),
